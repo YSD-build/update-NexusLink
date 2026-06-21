223ff7d8d03b643fde1a248157fb431294ace12f
YSD-build <ysd-build@example.com>
2026-06-22 07:35:26 +0800

    v0.4.0 - 全站UI重做，流畅体验升级

--- api/version.php
+++ api/version.php
@@ -4,6 +4,6 @@
  * 这个文件会随更新自动覆盖，请勿修改
  */
 
-define('CURRENT_VERSION', 'v0.3.2');
-define('VERSION_CODE', 2026062203);
+define('CURRENT_VERSION', 'v0.4.0');
+define('VERSION_CODE', 2026062204);
 define('VERSION_DATE', '2026-06-22');
