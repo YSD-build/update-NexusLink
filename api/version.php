af4b104ee3347df2b084f6253c1e8f9e16d0dc3d
YSD-build <ysd-build@example.com>
2026-06-23 01:48:18 +0800

    v0.7.4 - 替换emoji图标为纯CSS图标

--- api/version.php
+++ api/version.php
@@ -4,6 +4,6 @@
  * 随更新自动覆盖
  */
 
-define('CURRENT_VERSION', 'v0.7.3');
-define('VERSION_CODE', 2026062303);
+define('CURRENT_VERSION', 'v0.7.4');
+define('VERSION_CODE', 2026062304);
 define('VERSION_DATE', '2026-06-23');
