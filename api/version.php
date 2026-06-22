b0f654393a015ef9a84efaf143c031aecd11d87b
YSD-build <ysd-build@example.com>
2026-06-22 22:28:21 +0800

    更新版本号到v0.5.2

--- api/version.php
+++ api/version.php
@@ -4,6 +4,6 @@
  * 随更新自动覆盖
  */
 
-define('CURRENT_VERSION', 'v0.5.1');
-define('VERSION_CODE', 2026062216);
+define('CURRENT_VERSION', 'v0.5.2');
+define('VERSION_CODE', 2026062217);
 define('VERSION_DATE', '2026-06-22');
